5d4abf6ebd94a8029a0d8395b502a042d0e211c6
arezoo <93465403+arezoomohammadi22@users.noreply.github.com>
2024-05-19 15:37:14 +0330

    Update app.py

--- Docker/app.py
+++ Docker/app.py
@@ -1,21 +1,26 @@
 import time
 import redis
 from flask import Flask
+
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
+
 def get_hit_count():
- retries = 5
- while True:
- try:
- return cache.incr('hits')
- except redis.exceptions.ConnectionError as exc:
- if retries == 0:
- raise exc
- retries -= 1
- time.sleep(0.5)
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
 @app.route('/')
 def hello():
- count = get_hit_count()
- return 'Hello World! I have been seen {} times.\n'.format(count)
+    count = get_hit_count()
+    return f'Hello World! I have been seen {count} times.\n'
+
 if __name__ == '__main__':
- app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
+
