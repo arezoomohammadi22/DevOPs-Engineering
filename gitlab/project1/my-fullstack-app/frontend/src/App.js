d90274e523db148e581e4e6e9beccb6e18495f76
arezoo <93465403+arezoomohammadi22@users.noreply.github.com>
2024-06-30 00:08:47 +0330

    Create App.js

---
+++ gitlab/project1/my-fullstack-app/frontend/src/App.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/message')
+      .then(response => response.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Frontend</h1>
+        <p>{message}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
