7d4b1959e9d75713b362bd1331973bfc23448d9a
arezoo <93465403+arezoomohammadi22@users.noreply.github.com>
2025-07-19 14:34:18 +0330

    Create App.jsx

---
+++ k8s/ingress/example02-k8s-fullstack-ingress-demo/frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>🌐 Frontend is working</h1>
+      <p>Welcome to the React frontend served at <code>/</code></p>
+    </div>
+  );
+}
+export default App;
