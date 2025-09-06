5e68f11d8231e963ddce781feb68192b81dfce80
arezoo <93465403+arezoomohammadi22@users.noreply.github.com>
2025-09-06 19:43:08 +0330

    Update main.tf

--- terraform/providers/docker-provider/main.tf
+++ terraform/providers/docker-provider/main.tf
@@ -1,9 +1,14 @@
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name = "nginx:latest"
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "3.6.2"
+    }
+  }
 }
 
+provider "docker" {
+}
 resource "docker_container" "web" {
   name  = "nginx-demo"
   image = docker_image.nginx.image_id
